ae01ee9112146e0bc7561c26808ce67d9575ef6c
QuinnChrest <quinnchr@gmail.com>
2020-03-04 11:52:24 -0600

    Updates

---
+++ precache-manifest.2ab3ea34dc0f28567db3623449481399.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "359672b166ad38e728ada6296db1a840",
+    "url": "/Pokedex/index.html"
+  },
+  {
+    "revision": "beb22be9565914843fae",
+    "url": "/Pokedex/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "30fe9acf2adc8dff12b8",
+    "url": "/Pokedex/static/css/main.035b2386.chunk.css"
+  },
+  {
+    "revision": "beb22be9565914843fae",
+    "url": "/Pokedex/static/js/2.915b2fe5.chunk.js"
+  },
+  {
+    "revision": "d2dd3abbc5334163bef8ff73ba5218e3",
+    "url": "/Pokedex/static/js/2.915b2fe5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "30fe9acf2adc8dff12b8",
+    "url": "/Pokedex/static/js/main.67d34315.chunk.js"
+  },
+  {
+    "revision": "ff197dce04860dd8e5a6",
+    "url": "/Pokedex/static/js/runtime-main.9adfd13f.js"
+  }
+]);
